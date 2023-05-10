d562260490d6460676a8aad5a41089d93210e489
palrishabh <58916474+rishabh-art@users.noreply.github.com>
2023-05-10 18:11:27 +0530

    change on homeScript.js

---
+++ homeScript.js
@@ -0,0 +1,15 @@
+const apiKey = 'XWXORHP1704KVJ0K';
+      const channelId = '2141717';
+      const fieldId = '1';
+
+      const url = `https://api.thingspeak.com/channels/${channelId}/fields/${fieldId}/last.json?api_key=${apiKey}`;
+
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const value = data[fieldId];
+          const valueElement = document.getElementById('value');
+          valueElement.textContent = value;
+        })
+        .catch(error => console.error(error));
+        alert(value)
